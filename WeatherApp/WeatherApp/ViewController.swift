441faaf4606b74c9468e65d4708977c0873c1e78
httpszoom <61495918+httpszoom@users.noreply.github.com>
2021-03-26 17:55:11 +0100

    Add files via upload

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Hubert Page on 25/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var dayOneLabel: UILabel!
+    
+    @IBOutlet weak var temperatureOneLabel: UILabel!
+    
+    @IBOutlet weak var tempsOneLabel: UILabel!
+    
+    @IBOutlet weak var iconeOneLabel: UIImageView!
+    
+    @IBOutlet var backgroundLabel: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
