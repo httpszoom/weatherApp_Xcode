82992d10c840058755d8ccd82564058fd512c834
Hubert <hubert.page01@gmail.com>
2021-03-29 19:37:03 +0200

    Données API

---
+++ WeatherApp/WeatherApp/JSONDonnees.swift
@@ -0,0 +1,8 @@
+//
+//  JSONDonnees.swift
+//  WeatherApp
+//
+//  Created by Hubert Page on 27/03/2021.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherApp/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  WeatherApp
+//
+//  Created by Hubert Page on 27/03/2021.
+//
+
+import Foundation
